bde50d627cf1ed39e37e2ff4e4db195499663cd1
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-11-12 21:32:33 +0100

    Add files via upload

---
+++ biblioteka/Forms/Pocetna.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace biblioteka
+{
+    partial class Pocetna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.korisniciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pregledKorisnikaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dodajKorisnikaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.knjigeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pregledKnjigaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dodajKnjiguToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.mnuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.korisniciToolStripMenuItem,
+            this.knjigeToolStripMenuItem});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(1344, 42);
+            this.mnuStrip.TabIndex = 0;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // korisniciToolStripMenuItem
+            // 
+            this.korisniciToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pregledKorisnikaToolStripMenuItem,
+            this.dodajKorisnikaToolStripMenuItem});
+            this.korisniciToolStripMenuItem.Name = "korisniciToolStripMenuItem";
+            this.korisniciToolStripMenuItem.Size = new System.Drawing.Size(122, 38);
+            this.korisniciToolStripMenuItem.Text = "Korisnici";
+            // 
+            // pregledKorisnikaToolStripMenuItem
+            // 
+            this.pregledKorisnikaToolStripMenuItem.Name = "pregledKorisnikaToolStripMenuItem";
+            this.pregledKorisnikaToolStripMenuItem.Size = new System.Drawing.Size(329, 44);
+            this.pregledKorisnikaToolStripMenuItem.Text = "Pregled korisnika";
+            // 
+            // dodajKorisnikaToolStripMenuItem
+            // 
+            this.dodajKorisnikaToolStripMenuItem.Name = "dodajKorisnikaToolStripMenuItem";
+            this.dodajKorisnikaToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.dodajKorisnikaToolStripMenuItem.Text = "Dodaj korisnika";
+            this.dodajKorisnikaToolStripMenuItem.Click += new System.EventHandler(this.dodajKorisnikaToolStripMenuItem_Click);
+            // 
+            // knjigeToolStripMenuItem
+            // 
+            this.knjigeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pregledKnjigaToolStripMenuItem,
+            this.dodajKnjiguToolStripMenuItem});
+            this.knjigeToolStripMenuItem.Name = "knjigeToolStripMenuItem";
+            this.knjigeToolStripMenuItem.Size = new System.Drawing.Size(101, 38);
+            this.knjigeToolStripMenuItem.Text = "Knjige";
+            // 
+            // pregledKnjigaToolStripMenuItem
+            // 
+            this.pregledKnjigaToolStripMenuItem.Name = "pregledKnjigaToolStripMenuItem";
+            this.pregledKnjigaToolStripMenuItem.Size = new System.Drawing.Size(299, 44);
+            this.pregledKnjigaToolStripMenuItem.Text = "Pregled knjiga";
+            // 
+            // dodajKnjiguToolStripMenuItem
+            // 
+            this.dodajKnjiguToolStripMenuItem.Name = "dodajKnjiguToolStripMenuItem";
+            this.dodajKnjiguToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.dodajKnjiguToolStripMenuItem.Text = "Dodaj knjigu";
+            this.dodajKnjiguToolStripMenuItem.Click += new System.EventHandler(this.dodajKnjiguToolStripMenuItem_Click);
+            // 
+            // Pocetna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 906);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "Pocetna";
+            this.Text = "Form1";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem korisniciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pregledKorisnikaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dodajKorisnikaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem knjigeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pregledKnjigaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dodajKnjiguToolStripMenuItem;
+    }
+}
+
---
+++ biblioteka/Forms/Pocetna.cs
@@ -0,0 +1,35 @@
+﻿using biblioteka.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace biblioteka
+{
+    public partial class Pocetna : Form
+    {
+        public Pocetna()
+        {
+            InitializeComponent();
+        }
+
+        private void dodajKorisnikaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DodajKorisnika noviKorisnik = new DodajKorisnika();
+
+            noviKorisnik.ShowDialog(Owner);
+        }
+
+        private void dodajKnjiguToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DodajKnjigu novaKnjiga = new DodajKnjigu();
+
+            novaKnjiga.ShowDialog(Owner);
+        }
+    }
+}
